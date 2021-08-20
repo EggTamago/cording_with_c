b3b49396f6ec7c8315d7a6354a5b11cb332a03a0
Keiichiro Tani <keiichiro.tani.dx@outlook.jp>
2021-08-20 23:46:37 +0900

    create eventlog

---
+++ create-eventlog/create-eventlog/create-eventlog.cpp
@@ -0,0 +1,40 @@
+﻿// 
+// windows event logへの書き込み方法
+// 参考：https://docs.microsoft.com/ja-jp/windows/win32/eventlog/reporting-an-event
+//
+
+#include <iostream>
+#include <windows.h>
+#include <winbase.h>
+
+#define EventSource L"RAS_INFO"
+
+int main()
+{
+
+	HANDLE hEvent;
+	BOOL Status;
+
+	/* イベントログのオープン */
+	hEvent = RegisterEventSource(NULL, EventSource);
+	if (hEvent == NULL) {
+		printf("can not create event handle\n");
+	};
+
+	/* イベントログへ書き込み*/
+	Status = ReportEvent(
+		hEvent,
+		EVENTLOG_INFORMATION_TYPE,
+		0,
+		MSG_MEMORY_ERROR,
+		NULL,
+		0,
+		0,
+		NULL,
+		NULL);
+
+	/* イベントログのクローズ */
+	DeregisterEventSource(hEvent);
+}
+
+
