cfcd5839f77c0c7bd1ee0b2fb2412f3c4ee7fa2e
Keiichiro Tani <keiichiro.tani.dx@outlook.jp>
2021-08-22 00:09:06 +0900

    add reference

--- create-eventlog/create-eventlog/create-eventlog.cpp
+++ create-eventlog/create-eventlog/create-eventlog.cpp
@@ -1,7 +1,9 @@
 ﻿// 
 // windows event logへの書き込み方法
 // 参考：https://docs.microsoft.com/ja-jp/windows/win32/eventlog/reporting-an-event
+// 参考 : https://togarasi.wordpress.com/2008/05/24/%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88%E3%83%AD%E3%82%B0%E3%81%A7%E4%BD%BF%E3%81%86%E3%83%A1%E3%83%83%E3%82%BB%E3%83%BC%E3%82%B8%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB-dll%E3%82%92%E4%BD%9C%E6%88%90/
 //
+
 
 #include <iostream>
 #include <windows.h>
@@ -9,9 +11,9 @@
 
 #include "../event-resource/provider.h"
 
-#define EventSource L"RAS_INFO"
+#define EventSource  L"RAS_INFO"
 
-int main()
+void main(void)
 {
 
 	HANDLE hEvent;
@@ -37,6 +39,8 @@ int main()
 
 	/* イベントログのクローズ */
 	DeregisterEventSource(hEvent);
+
+	Sleep(3 * 1000);
 }
 
 
