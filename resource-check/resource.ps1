e698ffc07b3effeda90a3aaca820dc341a31bc91
Keiichiro Tani <keiichiro.tani.dx@outlook.jp>
2021-08-27 00:10:11 +0900

    check pc resouce by ps1"

--- resource-check/resource.ps1
+++ resource-check/resource.ps1
@@ -1,7 +1,27 @@
+# register event resource 
+New-EventLog -LogName Application -Source "RAS_INFO_PS" -ErrorAction SilentlyContinue
+
+while ($true) {
+    # メモリ使用率
+    $usingMemory = Get-WmiObject Win32_OperatingSystem | %{(($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)/$_.TotalVisibleMemorySize) * 100}
+    $usingMemory
+}
+
 # メモリ使用率
 $usingMemory = Get-WmiObject Win32_OperatingSystem | %{(($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)/$_.TotalVisibleMemorySize) * 100}
-$usingMemory
+
+
+if (0 -lt $usingMemory -lt 30) {
+    echo "under 50"
+    Write-EventLog -LogName Application -Source "RAS_INFO_PS" -EventId 123 -EntryType Information -Category 0 -Message "メモリこれくらい使ってるよ $usingMemory"
+} elseif (30 -le $usingMemory -and $usingMemory -lt 80) {
+    echo "between 30 to 80"
+    Write-EventLog -LogName Application -Source "RAS_INFO_PS" -EventId 456 -EntryType Information -Category 0 -Message "メモリこれくらい使ってるよ $usingMemory"
+} else {
+    echo "why?"
+}
+
 
 # ディスク使用率 
 $unusingDisk = Get-PSDrive -PSProvider FileSystem
-$unusingDisk | Select-Object "Free" |%{((($_.Free) / 1024) / 1024) / 1024}
+$unusingDisk | Select-Object "Free" | %{((($_.Free) / 1024) / 1024) / 1024} # byte to GB
