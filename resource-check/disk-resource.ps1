c96313d88c24c2a7d3c0b0c4d523b0df053e254f
Keiichiro Tani <keiichiro.tani.dx@outlook.jp>
2021-09-09 23:52:16 +0900

    resource check

---
+++ resource-check/disk-resource.ps1
@@ -0,0 +1,41 @@
+
+#
+# disk resource check
+#
+
+# ディレクトリがなければ作成
+if (-Not(Test-Path C:\testdir)) {
+    New-Item C:\testdir -ItemType Directory
+}
+
+# register event resource 
+New-EventLog -LogName Application -Source "RAS_INFO_PS" -ErrorAction SilentlyContinue
+
+
+while ($true) {
+
+    # ディスク情報取得 
+    $diskInfo = Get-PSDrive -PSProvider FileSystem
+
+    # ディスク未使用量
+    $unusingDisk = $diskInfo | Select-Object "Free" | %{((($_.Free) / 1024) / 1024) / 1024} # byte to GB
+
+    # ディスク使用量
+    $usedDisk = $diskInfo | Select-Object "Used" | %{((($_.Used) / 1024) / 1024) / 1024} # byte to GB
+
+    # ディスク使用率
+    $zeroDivCheck = $usedDisk + $unusingDisk
+    if ( $zeroDivCheck -eq 0) {
+        echo "zero division error!"
+        sleep(3)
+        continue
+    } else {
+        $usedDiskPercent = (($usedDisk / ($usedDisk + $unusingDisk)) * 100)
+    }
+
+    $usedDiskPercent
+
+    sleep(3)
+}
+
+
---
+++ resource-check/memory-resource.ps1
@@ -0,0 +1,30 @@
+
+#
+# memory resource check
+#
+
+# ディレクトリがなければ作成
+if (-Not(Test-Path C:\testdir)) {
+    New-Item C:\testdir -ItemType Directory
+}
+
+# register event resource 
+New-EventLog -LogName Application -Source "RAS_INFO_PS" -ErrorAction SilentlyContinue
+
+while ($true) {
+    # メモリ使用率
+    $usingMemory = Get-WmiObject Win32_OperatingSystem | %{(($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)/$_.TotalVisibleMemorySize) * 100}
+
+    if (0 -lt $usingMemory -lt 30) {
+        echo "under 30"
+        Write-EventLog -LogName Application -Source "RAS_INFO_PS" -EventId 123 -EntryType Information -Category 0 -Message "メモリこれくらい使ってるよ $usingMemory"
+    } elseif (30 -le $usingMemory -and $usingMemory -lt 80) {
+        echo "between 30 to 80"
+        Write-EventLog -LogName Application -Source "RAS_INFO_PS" -EventId 456 -EntryType Information -Category 0 -Message "メモリこれくらい使ってるよ $usingMemory"
+    } else {
+        echo "high memory usage"
+    }
+    
+    sleep(3)
+}
+
