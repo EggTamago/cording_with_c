1994589a6cfbc6daa11c009c24e16b05e12387c5
Keiichiro Tani <keiichiro.tani.dx@outlook.jp>
2021-09-14 22:34:38 +0900

    csv split script

---
+++ csv-remake/csv-remake.ps1
@@ -0,0 +1,26 @@
+﻿
+#
+# Split by csv row number by powershell script
+#
+
+# get current directry
+$currentpath = (Convert-Path .)
+$currentpath.GetType().FullName
+
+# if no sub directory, make directory
+if (!(Test-Path $currentpath\sub)) {
+    New-Item $currentpath\sub -ItemType Directory > $null
+}
+
+
+# get header info
+$header = Get-Content iris.csv -TotalCount 1
+
+# make files
+$i=0
+Get-Content iris.csv -ReadCount 10 | % {$_ | Out-File "$currentpath\sub\test_$i.csv" -Encoding UTF8 -Append; $i++}
+
+# add header info
+
+
+
