817b5c296de95bac543221121200456f47ab3be1
Keiichiro Tani <keiichiro.tani.dx@outlook.jp>
2021-09-18 07:46:47 +0900

    排他制御

---
+++ mutex/mutex/mutex/mutex.cpp
@@ -0,0 +1,56 @@
+﻿//
+// 排他制御サンプル
+// スレッド間とプロセス間の排他制御がある
+//
+
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+
+int processMutex()
+{
+    HANDLE hHandle;
+    HANDLE h;
+    BOOL result;
+
+    /* global\\をつけるとすべてのユーザに対して排他制御. global抜きだとsystemユーザのみに排他制御 */
+    hHandle = CreateMutex(NULL, TRUE, "Global\\sample");
+
+    /* 作成したハンドルをopen */
+    h = OpenMutex(MUTEX_ALL_ACCESS, FALSE, "Global\\sample");
+
+    /* ハンドルがシグナル状態＝使用できる状態になるまで待つ */
+    WaitForSingleObject(h, INFINITE);
+
+    /*
+    
+    ここに処理を記載する
+    
+    */
+
+
+    /* mutexとhandleの解放 */
+    result = ReleaseMutex(h);
+    if (!result) {
+        printf("fail to release mutex handle\n");
+    }
+
+    result = CloseHandle(hHandle);
+    if (!result) {
+        printf("fail to close handle\n");
+    }
+
+    return 0;
+}
+
+
+int threadMutex()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
